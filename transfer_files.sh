c50252b6d871211965e5a5e59b07a123b9a77c77
angela-shen <136782411+angela-shen@users.noreply.github.com>
2025-05-09 02:56:36 -0700

    Adding files

--- transfer_files.sh
+++ transfer_files.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-scp -i DSCKEY.pem subset.csv ubuntu@$1:~
-scp -i DSCKEY.pem PA0.py ubuntu@$1:~
+scp -i DSCKEY.pem PA1/starter_notebook.ipynb ubuntu@$1:~
--- update_py.sh
+++ update_py.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp -i DSCKEY.pem ubuntu@$1:*.py .
+scp -i DSCKEY.pem ubuntu@$1:starter_notebook.ipynb PA1/starter_notebook.ipynb
